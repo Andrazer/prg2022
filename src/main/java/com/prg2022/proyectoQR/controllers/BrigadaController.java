e36d0627f02490eee7aec11221fa8ea21ed30d4e
James <extremo_ve@hotmail.com>
2022-05-16 17:39:22 +0200

    Agregar brigadas listo

--- src/main/java/com/prg2022/proyectoQR/controllers/BrigadaController.java
+++ src/main/java/com/prg2022/proyectoQR/controllers/BrigadaController.java
@@ -3,6 +3,8 @@ package com.prg2022.proyectoQR.controllers;
 import java.util.Collection;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +12,16 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.prg2022.proyectoQR.Repository.BrigadaRepository;
 import com.prg2022.proyectoQR.modelos.Brigada;
+import com.prg2022.proyectoQR.payload.request.AddBrigadaRequest;
+import com.prg2022.proyectoQR.payload.response.MessageResponse;
 
 @Controller
 @RequestMapping("/brigada")
@@ -37,6 +43,47 @@ public class BrigadaController {
     public ResponseEntity<Collection<Brigada>> findAll() {
         List<Brigada> brigada = (List<Brigada>) brepository.findAll();
         return ResponseEntity.ok(brigada);
+    }
+    @PostMapping("/add")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody AddBrigadaRequest addbrigadarequest) {
+      if (!brepository.findByDescripcion(addbrigadarequest.getDescripcion()).isEmpty()) {
+        return ResponseEntity.badRequest().body(new MessageResponse("Error: Esa Brigada ya existe"));
+      }
+      brepository.save(new Brigada(addbrigadarequest.getDescripcion()));
+      // Create new user's account
+      /*
+      Alumno user = new Alumno(signUpRequest.getUsername(),
+                           encoder.encode(signUpRequest.getPassword()),
+                           signUpRequest.getBrigada());
+      Set<String> strRoles = signUpRequest.getRole();
+      Set<Role> roles = new HashSet<>();
+      if (strRoles == null) {
+        Role userRole = roleRepository.findByDescripcion(EnumRole.ROLE_USER)
+            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+        roles.add(userRole);
+      } else {
+        strRoles.forEach(role -> {
+          switch (role) {
+          case "admin":
+            Role adminRole = roleRepository.findByDescripcion(EnumRole.ROLE_ADMIN)
+                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(adminRole);
+            break;
+          case "mod":
+            Role modRole = roleRepository.findByDescripcion(EnumRole.ROLE_MODERATOR)
+                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(modRole);
+            break;
+          default:
+            Role userRole = roleRepository.findByDescripcion(EnumRole.ROLE_USER)
+                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+          }
+        });
+      }
+      user.setRoles(roles);
+      userRepository.save(user);*/
+      return ResponseEntity.ok(new MessageResponse("Brigada registrada!"));
     }
 }
 
---
+++ src/main/java/com/prg2022/proyectoQR/payload/request/AddBrigadaRequest.java
@@ -0,0 +1,18 @@
+package com.prg2022.proyectoQR.payload.request;
+
+import javax.validation.constraints.*;
+
+
+public class AddBrigadaRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String descripcion;  
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+ 
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
