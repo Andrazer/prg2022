6adfd2ce7b87590b8818191c5dc3927aaa595890
james <james@local>
2022-06-17 10:53:41 +0200

    movimientos

--- src/main/java/com/prg2022/proyectoQR/Repository/UsuarioRepository.java
+++ src/main/java/com/prg2022/proyectoQR/Repository/UsuarioRepository.java
@@ -8,13 +8,20 @@ import com.prg2022.proyectoQR.modelos.Brigada;
 import com.prg2022.proyectoQR.modelos.Usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>, JpaSpecificationExecutor<Usuario> {
   Optional<Usuario> findByDni(String dni);
   Boolean existsByDni(String dni);
   List<Usuario> findByBrigada(Brigada brigada);
   Optional<Usuario> findBynumeroDeBrigadaIs( String text );
+  
+  @Query(value = "select count(usuarios.id) as total, count(CASE WHEN abordo THEN 1 END ) as Abordo, "+
+  "count(CASE WHEN not abordo THEN 0 END ) as Entierra, Brigada_id as brigadaid, brigadas.descripcion as nombre "+
+  " from usuarios,brigadas where usuarios.brigada_id=brigadas.id and brigada_id>1 group by brigada_id", nativeQuery = true)
+  public List<?> buscajames();
 }
--- src/main/java/com/prg2022/proyectoQR/controllers/MovimientosController.java
+++ src/main/java/com/prg2022/proyectoQR/controllers/MovimientosController.java
@@ -1,5 +1,7 @@
 package com.prg2022.proyectoQR.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.prg2022.proyectoQR.Repository.MovimientoRepository;
+import com.prg2022.proyectoQR.Repository.UsuarioRepository;
 
 @Controller
 
@@ -17,11 +20,16 @@ import com.prg2022.proyectoQR.Repository.MovimientoRepository;
 public class MovimientosController {
     @Autowired
     private MovimientoRepository mrepository;
+
+    @Autowired
+    private UsuarioRepository urepository;
     
     @GetMapping("")
     public ModelAndView getLector(ModelAndView modelAndView, Model pagina) {
+
         pagina.addAttribute("titulo", "Movimientos");
-        //modelAndView.addObject("pagina", pagina);
+        pagina.addAttribute("cuenta", urepository.buscajames());
+        modelAndView.addObject("pagina", pagina);
         modelAndView.setViewName("movimientos");
         return modelAndView;
     }
